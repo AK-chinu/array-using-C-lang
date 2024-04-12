2e007e70ae0e09c8903e1ab0692e909c8402dabc
chinmaya padhi <116653419+AK-chinu@users.noreply.github.com>
2024-04-12 16:29:13 +0530

    Add files via upload

---
+++ problem6.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int arr[6]={2,5,3,6,1,8};
+    int count=0;
+    int x=10;
+    for(int i=0;i<6;i++)
+    {
+        for(int j=i+1;j<6;j++)
+        {
+            for(int k=j+1;k<6;k++)
+            {
+                if((arr[i]+arr[j]+arr[k])==x)
+                {
+                    count=count+1;
+                }
+            }
+        }
+    }
+    printf("%d",count);
+}
